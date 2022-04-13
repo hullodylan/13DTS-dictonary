5cf96c3bb73cb92f1b9429c28202b9fb55ce9ad6
18004 <18004@wc.school.nz>
2022-04-14 11:10:36 +1200

    working app.py with category.html and displays correct words with category

--- internal1/Maori Dictionary/app.py
+++ internal1/Maori Dictionary/app.py
@@ -25,15 +25,13 @@ def create_connection(db_file):
 def render_homepage():
     if request.method == 'POST':
         cat_name =request.form.get('cat_name').strip().lower()
-        user_id = session['userid']
-        timestamp = datetime.now()
         con = create_connection(database)
 
-        query = "INSERT INTO category(id, cat_name, user_id, timestamp ) VALUES(NULL,?, ?, ?)"
+        query = "INSERT INTO category(id, cat_name) VALUES(NULL,?)"
         cur = con.cursor()  # You need this line next
 
         try:
-            cur.execute(query, (cat_name, user_id, timestamp))  # this line actually executes the query
+            cur.execute(query, (cat_name, ))  # this line actually executes the query
         except sqlite3.IntegrityError:
             return redirect('/?error=category+is+already+used')
         con.commit()
@@ -50,23 +48,15 @@ def render_homepage():
 def render_category_page(cat_id):
     con = create_connection(database)
 
-    query = "SELECT maori, english, image FROM wordbank"
+    query = "SELECT cat_id, maori, english, image FROM wordbank"
 
     cur = con.cursor()  # You need this line next
     cur.execute(query)  # this line actually executes the query
     word_ids = cur.fetchall()  # puts the results into a list usable in python
-
-    unique_cat_id = list(set(word_ids))
-
-
-    for i in range(len(unique_cat_id)):
-        cat_count = word_ids.count(unique_cat_id[i])
-        unique_cat_id[i] = [unique_cat_id[i], cat_count]
-    print(unique_cat_id)
-
-
     con.close()
-    return render_template('category.html', wordbank=word_ids, logged_in=is_logged_in(), categories=categories())
+
+
+    return render_template('category.html', wordbank=word_ids, logged_in=is_logged_in(), categories=categories(), category_id=int(cat_id))
 
 
 
